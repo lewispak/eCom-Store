40526fece841e7ffbd4e3d303ed3afb098801f93
Lewis Pakoti <67088984+lewispak@users.noreply.github.com>
2024-01-12 11:15:35 +1300

    added routing and basic page templating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,43 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+
+const Layout = () =>{
+  return (
+    <div className="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <span>Home</span>,
-  },
-  {
-    path: "/products/:id",
-    element: <span>Category</span>,
-  },
-  {
-    path: "/product/:id",
-    element: <span>Product</span>,
+    path:"/",
+    element: <Layout/>,
+    children: [
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/products/:id",
+        element:<Products/>
+      },
+      {
+        path:"/product/:id",
+        element:<Product/>
+      }
+    ]
   }
 ]);
 
