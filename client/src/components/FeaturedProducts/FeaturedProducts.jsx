24ae74abef82ac43751579e7b7ebcf2f1a0cb656
Lewis Pakoti <67088984+lewispak@users.noreply.github.com>
2024-01-17 16:56:12 +1300

    used strapi filters to filter products in featured and trending section of home page

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,35 +1,12 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import axios from 'axios';
-
+import React from 'react';
 import Card from '../Card/Card';
-
 import "./featuredProducts.scss";
+import useFetch  from "../../hooks/useFetch.js";
 
-const FeaturedProducts = ({type}) => {
-
-  const [data, setData] = useState([])
-
-  useEffect(()=>{
-    const fetchData = async () => {
-      try{
-        const res = await axios.get(
-          import.meta.env.REACT_APP_API_URL + "/products?populate=*", 
-          {
-            headers: {
-              Authorization: "bearer " + import.meta.env.REACT_APP_API_TOKEN,
-            },
-          }
-        );
-        setData(res.data.data)
-      } catch (err){
-        console.log(err)
-      }
-    };
-    fetchData();
-  },[])
-
-  console.log(data)
+const FeaturedProducts = ({ type }) => {
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
 
   return (
     <div className="featuredProducts">
@@ -45,9 +22,11 @@ const FeaturedProducts = ({type}) => {
       </div>
 
       <div className="bottom">
-        {data.map(item=>(
-          <Card item={item} key={item.id}/>
-        ))}
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "Loading..."
+          : data?.map((item) => <Card item={item} key={item.id}/>)}
       </div>
     </div>  
   );
